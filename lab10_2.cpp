620ab729a651d4d58017e2968cc022188da3c224
Unknown <nichapat18102541@gmail.com>
2018-01-29 18:22:04 +0700

    เสร็จแล้ว ข้อที่ 2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,27 @@
-int main() {
+#include <iostream>
+#include <fstream>
+using namespace std;
         /* code */
+        /*
+    เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรดังกล่าว
+    โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรมและแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
+    */
+    float cArea (float r){
+        float area;
+        area = 3.1416*r*r;
+
+        return area;}
+
+int main() {
+    float r, cmd;
+    do {
+        cin >> cmd;
+        if (cmd == 1) {
+            cin >> r;
+            cout << cArea (r) <<endl;
+        } else {break;}
+        } while(1);
         return 0;
-}
+    }
+
+
